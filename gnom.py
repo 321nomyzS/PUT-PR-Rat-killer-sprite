a97b58facf38aa351b02acf63274f1c6060991c5
321nomyzS <balinskiszymon@gmail.com>
2023-05-18 18:00:34 +0200

    Changes in gnom and skrzat

--- gnom.py
+++ gnom.py
@@ -17,11 +17,12 @@ def gnome_code(comm, G, ac):
     wait_queue = []
     ack_counter = 0
     current_state = "REST"
-    lamport_clock = 0
 
     # Ustawienia MPI
     rank = comm.Get_rank()
     size = comm.Get_size()
+
+    lamport_clock = 0
 
     # Ustawianie koloru tekstów
     colorama.init()
@@ -30,7 +31,7 @@ def gnome_code(comm, G, ac):
 
     while True:
         if current_state == "REST":
-            print(f"[GNOM:{rank}] Jestem w stanie REST")
+            print(f"[GNOM:{rank}|{lamport_clock}] Jestem w stanie REST")
             messages = get_messages(comm, rank, size) # TO DO: Wiadomości powinny być odbierane w pętli do jakiegoś momentu.
             # Możliwe, że wiadomość dojdzie do procesu w momencie, jak proces będzie linijkę niżej. Wtedy wiadomość nie zostanie uwzględniona
 
@@ -65,45 +66,45 @@ def gnome_code(comm, G, ac):
                 current_state = "WAIT"
 
         if current_state == "WAIT":
-            print(f"[GNOM:{rank}] Jestem w stanie WAIT")
-            messages = get_messages(comm, rank, size) # TO DO: Wiadomości powinny być odbierane w pętli do jakiegoś momentu.
+            print(f"[GNOM:{rank}|{lamport_clock}] Jestem w stanie WAIT")
+            # TO DO: Wiadomości powinny być odbierane w pętli do jakiegoś momentu.
             # Możliwe, że wiadomość dojdzie do procesu w momencie, jak proces będzie linijkę niżej. Wtedy wiadomość nie zostanie uwzględniona
+            while ack_counter < G - ac:
+                messages = get_messages(comm, rank, size)
+                for message in messages:
+                    message_author = message[0]
+                    message_type = message[1].split()[0]
 
-            for message in messages:
-                message_author = message[0]
-                message_type = message[1].split()[0]
+                    if message_type == "sREQ":
+                        continue
 
-                if message_type == "sREQ":
-                    continue
+                    elif message_type == "gREQ":
+                        # TO DO: Wykorzystać zegar Lamporta w wysyłaniu i odbieraniu wiadomości
+                        message_clock = int(message[1].split()[1])
+                        if lamport_clock <= message_clock:
+                            lamport_clock = message_clock + 1
+                            comm.send("ACK", dest=message_author)
+                        else:
+                            lamport_clock += 1
+                            wait_queue.append(message_author)
 
-                elif message_type == "gREQ":
-                    # TO DO: Wykorzystać zegar Lamporta w wysyłaniu i odbieraniu wiadomości
-                    message_clock = int(message[1].split()[1])
-                    if lamport_clock < message_clock:
-                        lamport_clock = message_clock + 1
-                        comm.send("ACK", dest=message_author)
-                    else:   
-                        lamport_clock += 1 
-                        wait_queue.append(message_author)
+                    elif message_type == "ACK":
+                        ack_counter += 1
 
-                elif message_type == "ACK":
-                    ack_counter += 1
+                    elif message_type == "gCHG":
+                        ac -= 1
 
-                elif message_type == "gCHG":
-                    ac -= 1
-
-                elif message_type == "sCHG":
-                    ac += 1
+                    elif message_type == "sCHG":
+                        ac += 1
 
             # Przejście do stanu INSECTION
-            if ack_counter >= G - ac:
-                current_state = "INSECTION"
+            current_state = "INSECTION"
 
         if current_state == "INSECTION":
-            print(f"[GNOM:{rank}] Jestem w stanie INSECTION")
+            print(f"[GNOM:{rank}|{lamport_clock}] Jestem w stanie INSECTION")
             ac -= 1
             comm.bcast("gCHG", root=rank)
-            print(f"[GNOM:{rank}] Wysyłam wiadomość gCHG do wszystkich procesów")
+            print(f"[GNOM:{rank}|{lamport_clock}] Wysyłam wiadomość gCHG do wszystkich procesów")
 
             messages = get_messages(comm, rank, size)  # TO DO: Wiadomości powinny być odbierane w pętli do jakiegoś momentu.
             # Możliwe, że wiadomość dojdzie do procesu w momencie, jak proces będzie linijkę niżej. Wtedy wiadomość nie zostanie uwzględniona
@@ -130,6 +131,6 @@ def gnome_code(comm, G, ac):
             # Przejście do stanu REST
             for target_rank in wait_queue:
                 comm.send("ACK", dest=target_rank)
-                print(f"[GNOM:{rank}] Wysyłam wiadomość ACK do {target_rank}")
+                print(f"[GNOM:{rank}|{lamport_clock}] Wysyłam wiadomość ACK do {target_rank}")
             wait_queue = []
             current_state = "REST"
--- skrzat.py
+++ skrzat.py
@@ -5,11 +5,12 @@ from colorama import Fore
 def get_messages(comm, rank, size):
     messages = []
     for i in range(size):
-        status = MPI.Status()
-        if comm.iprobe(source=i, status=status):
-            message = comm.recv(source=i)
+        #status = MPI.Status()
+        #if comm.iprobe(source=i):
+        message = comm.recv(source=i)
+        if message is not None:
             print(f"[SKRZAT:{rank}] Otrzymałem wiadomość {message} od {i}")
-            messages.append((status.Get_source(), message))
+            messages.append((i, message))
     return messages
 
 def skrzat_code(comm, S, b):
@@ -17,11 +18,12 @@ def skrzat_code(comm, S, b):
     wait_queue = []
     ack_counter = 0
     current_state = "REST"
-    lamport_clock = 0
 
     # Ustawienia MPI
     rank = comm.Get_rank()
     size = comm.Get_size()
+
+    lamport_clock = 0
 
     # Ustawianie koloru tekstów
     colorama.init()
@@ -30,7 +32,7 @@ def skrzat_code(comm, S, b):
 
     while True:
         if current_state == "REST":
-            print(f"[SKRZAT:{rank}] Jestem w stanie REST")
+            print(f"[SKRZAT:{rank}|{lamport_clock}] Jestem w stanie REST")
             messages = get_messages(comm, rank, size) # TO DO: Wiadomości powinny być odbierane w pętli do jakiegoś momentu.
             # Możliwe, że wiadomość dojdzie do procesu w momencie, jak proces będzie linijkę niżej. Wtedy wiadomość nie zostanie uwzględniona
 
@@ -43,7 +45,7 @@ def skrzat_code(comm, S, b):
 
                 elif message_type == "sREQ":
                     comm.send("ACK", dest=message_author)
-                    print(f"[SKRZAT:{rank}] Odsyłam wiadomość ACK do {message_author}")
+                    print(f"[SKRZAT:{rank}|{lamport_clock}] Odsyłam wiadomość ACK do {message_author}")
 
                 elif message_type == "ACK":
                     continue
@@ -65,44 +67,46 @@ def skrzat_code(comm, S, b):
                 current_state = "WAIT"
 
         if current_state == "WAIT":
-            print(f"[SKRZAT:{rank}] Jestem w stanie WAIT")
-            messages = get_messages(comm, rank, size) # TO DO: Wiadomości powinny być odbierane w pętli do jakiegoś momentu.
+            print(f"[SKRZAT:{rank}|{lamport_clock}] Jestem w stanie WAIT")
+            # TO DO: Wiadomości powinny być odbierane w pętli do jakiegoś momentu.
             # Możliwe, że wiadomość dojdzie do procesu w momencie, jak proces będzie linijkę niżej. Wtedy wiadomość nie zostanie uwzględniona
 
-            for message in messages:
-                message_author = message[0]
-                message_type = message[1].split()[0]
+            while ack_counter < S - b:
+                messages = get_messages(comm, rank, size)
+                for message in messages:
+                    message_author = message[0]
+                    message_type = message[1].split()[0]
 
-                if message_type == "gREQ":
-                    continue
+                    if message_type == "gREQ":
+                        continue
 
-                elif message_type == "sREQ":
-                    message_clock = int(message[1].split()[1])
-                    if lamport_clock < message_clock:
-                        lamport_clock = message_clock + 1
-                        comm.send("ACK", dest=message_author)
-                    else:   
-                        lamport_clock += 1 
-                        wait_queue.append(message_author)
+                    elif message_type == "sREQ":
+                        message_clock = int(message[1].split()[1])
+                        if lamport_clock <= message_clock:
+                            lamport_clock = message_clock + 1
+                            comm.send("ACK", dest=message_author)
+                        else:
+                            lamport_clock += 1
+                            wait_queue.append(message_author)
 
-                elif message_type == "ACK":
-                    ack_counter += 1
+                    elif message_type == "ACK":
+                        ack_counter += 1
 
-                elif message_type == "sCHG":
-                    b -= 1
+                    elif message_type == "sCHG":
+                        b -= 1
 
-                elif message_type == "gCHG":
-                    b += 1
+                    elif message_type == "gCHG":
+                        b += 1
 
-            # Przejście do stanu INSECTION
-            if ack_counter >= S - b:
-                current_state = "INSECTION"
+                # Przejście do stanu INSECTION
+                # if ack_counter >= S - b:
+            current_state = "INSECTION"
 
         if current_state == "INSECTION":
-            print(f"[SKRZAT:{rank}] Jestem w stanie INSECTION")
+            print(f"[SKRZAT:{rank}|{lamport_clock}] Jestem w stanie INSECTION")
             b -= 1
             comm.bcast("sCHG", root=rank)
-            print(f"[SKRZAT:{rank}] Wysyłam wiadomość sCHG do wszystkich procesów")
+            print(f"[SKRZAT:{rank}|{lamport_clock}] Wysyłam wiadomość sCHG do wszystkich procesów")
 
             messages = get_messages(comm, rank, size) # TO DO: Wiadomości powinny być odbierane w pętli do jakiegoś momentu.
             # Możliwe, że wiadomość dojdzie do procesu w momencie, jak proces będzie linijkę niżej. Wtedy wiadomość nie zostanie uwzględniona
@@ -129,6 +133,6 @@ def skrzat_code(comm, S, b):
             # Przejście do stanu REST
             for target_rank in wait_queue:
                 comm.send("ACK", dest=target_rank)
-                print(f"[SKRZAT:{rank}] Wysyłam wiadomość ACK do {target_rank}")
+                print(f"[SKRZAT:{rank}|{lamport_clock}] Wysyłam wiadomość ACK do {target_rank}")
             wait_queue = []
             current_state = "REST"
